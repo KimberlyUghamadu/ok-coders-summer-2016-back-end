98491e951968c1e28e19caba2e2df4b94816d45c
KimberlyUghamadu <kugha2013@gmail.com>
2016-06-30 18:07:49 -0500

    Updated reader.js

--- exercises/week-3/node/template/reader.js
+++ exercises/week-3/node/template/reader.js
@@ -1,5 +1,11 @@
 
 var fs = require('fs');
 
+fs.readFile(process.argv[2], callback)
+
+function callback(err, data) {
+   if(err) throw err
+   console.log(data.toString())
+}
 // ...
 // and much more
