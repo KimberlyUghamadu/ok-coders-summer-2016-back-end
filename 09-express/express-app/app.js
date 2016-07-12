040f8013eb6bb29b04708528efa6b55615c57934
KimberlyUghamadu <kugha2013@gmail.com>
2016-07-12 18:07:38 -0500

    updated files

---
+++ 09-express/express-app/app.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.get('/music', function(req, res) {
+   res.json({beautiful: 'music'});
+})
+//app.listen(3000);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+//app.get('/music', function(req, res) {
+ //  res.send('GET music request to homepage');
+  // res.json({beautiful: 'music'});
+//});
+
+//app.post('/music', function(req, res) {
+  // res.send('POST song request to homepage');
+//});
+
+//app.put('/music', function(req, res) {
+  // res.send('PUT music request to homepage');
+//});
+
+//app.delete('/music', function(req, res) {
+  // res.send('DELETE song request to homepage');
+//});
+module.exports = app;
